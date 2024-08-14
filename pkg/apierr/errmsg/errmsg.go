4141e08546c5ab86b9256a6825d4acfa1e2b0679
obnahsgnaw <obnahsgnaw@qq.com>
2024-08-14 17:19:22 +0800

    refactor(x): x

--- pkg/apierr/errmsg/errmsg.go
+++ pkg/apierr/errmsg/errmsg.go
@@ -12,10 +12,18 @@ type LanguageMessage map[string]interface{}
 
 var (
 	En Language = "en"
+	Zh Language = "zh"
 )
 
 type LocalMessage struct {
-	data map[Language][]LanguageMessage
+	defLanguage Language
+	data        map[Language][]LanguageMessage
+}
+
+func (s *LocalMessage) SetDefaultLanguage(l Language) {
+	if l != "" {
+
+	}
 }
 
 func (s *LocalMessage) Load(lange Language, data []byte) error {
@@ -28,41 +36,52 @@ func (s *LocalMessage) Load(lange Language, data []byte) error {
 }
 
 func (s *LocalMessage) Translate(lang Language, target string, params ...interface{}) string {
-	if msgs, ok := s.data[lang]; ok {
-		for _, msg := range msgs {
-			if strings.ContainsAny(target, ".") {
-				targets := strings.Split(target, ".")
-				if v, ok := msg[targets[0]]; ok {
-					if vv, ok := v.(string); ok {
-						return fmt.Sprintf(vv, params...)
-					}
-					if vv, ok := v.(map[string]interface{}); ok {
-						if vvv, ok := vv[targets[1]]; ok {
-							if vvvv, ok := vvv.(string); ok {
-								return fmt.Sprintf(vvvv, params...)
-							}
+	if lang == "" {
+		lang = s.defLanguage
+	}
+	var msgs []LanguageMessage
+	var ok bool
+	if msgs, ok = s.data[lang]; !ok {
+		if msgs, ok = s.data[s.defLanguage]; !ok {
+			if msgs, ok = s.data[En]; !ok {
+				return target
+			}
+		}
+	}
+	for _, msg := range msgs {
+		if strings.ContainsAny(target, ".") {
+			targets := strings.Split(target, ".")
+			if v, ok := msg[targets[0]]; ok {
+				if vv, ok := v.(string); ok {
+					return fmt.Sprintf(vv, params...)
+				}
+				if vv, ok := v.(map[string]interface{}); ok {
+					if vvv, ok := vv[targets[1]]; ok {
+						if vvvv, ok := vvv.(string); ok {
+							return fmt.Sprintf(vvvv, params...)
 						}
 					}
 				}
-			} else {
-				if v, ok := msg[target]; ok {
-					if vv, ok := v.(string); ok {
-						return fmt.Sprintf(vv, params...)
-					}
-					if vv, ok := v.(map[string]interface{}); ok {
-						if vvv, ok := vv["default"]; ok {
-							if vvvv, ok := vvv.(string); ok {
-								return fmt.Sprintf(vvvv, params...)
-							}
+			}
+		} else {
+			if v, ok := msg[target]; ok {
+				if vv, ok := v.(string); ok {
+					return fmt.Sprintf(vv, params...)
+				}
+				if vv, ok := v.(map[string]interface{}); ok {
+					if vvv, ok := vv["default"]; ok {
+						if vvvv, ok := vvv.(string); ok {
+							return fmt.Sprintf(vvvv, params...)
 						}
 					}
 				}
 			}
 		}
 	}
+
 	return target
 }
 
 func New() *LocalMessage {
-	return &LocalMessage{data: make(map[Language][]LanguageMessage)}
+	return &LocalMessage{data: make(map[Language][]LanguageMessage), defLanguage: En}
 }
